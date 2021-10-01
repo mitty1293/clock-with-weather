6ee0ae5ed20f565aaa04656a21f87946bba71b7a
mitty1293 <k.fmichi@gmai.com>
2021-10-01 20:39:28 +0900

    型ヒント記載

--- flask/app/call_api.py
+++ flask/app/call_api.py
@@ -5,7 +5,6 @@ import requests, json
 
 def call_api(url: str) -> str:
     response = requests.get(url)
-    json_data = response.json()
+    json_data: dict = response.json()
     json_text: str = json.dumps(json_data, indent=4)
-    print(json_text)
     return json_text
