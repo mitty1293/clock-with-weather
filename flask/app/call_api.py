ff906b49e645e7d4b3d748ba58c624fbe78d8281
mitty1293 <k.fmichi@gmai.com>
2021-10-01 18:36:52 +0900

    アノテーション記載

--- flask/app/call_api.py
+++ flask/app/call_api.py
@@ -3,9 +3,9 @@
 
 import requests, json
 
-def call_api(url):
+def call_api(url: str) -> str:
     response = requests.get(url)
     json_data = response.json()
-    json_text = json.dumps(json_data, indent=4)
+    json_text: str = json.dumps(json_data, indent=4)
     print(json_text)
     return json_text
--- flask/app/gen_url.py
+++ flask/app/gen_url.py
@@ -3,7 +3,7 @@
 
 import keys
 
-def gen_url():
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&appid={key}"
-    formatted_url=url.format(key=keys.API_KEY, lat=keys.LAT, lon=keys.LON)
+def gen_url() -> str:
+    url: str = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&appid={key}"
+    formatted_url: str = url.format(key=keys.API_KEY, lat=keys.LAT, lon=keys.LON)
     return formatted_url
