fc6102dd5c43d29bf24aeff4bfd5fc280face8c0
mitty1293 <k.fmichi@gmai.com>
2021-10-02 06:58:33 +0900

    型ヒント記載

--- flask/app/call_api.py
+++ flask/app/call_api.py
@@ -2,9 +2,10 @@
 # _*_ coding: utf-8 _*_
 
 import requests, json
+from requests.models import Response
 
 def call_api(url: str) -> str:
-    response = requests.get(url)
+    response: Response = requests.get(url)
     json_data: dict = response.json()
     json_text: str = json.dumps(json_data, indent=4)
     return json_text
