1dbaa683e748c823bd7658a055a07e0074454ffa
mitty1293 <k.fmichi@gmai.com>
2021-10-03 05:24:27 +0900

    修正

--- flask/app/gen_url.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# _*_ coding: utf-8 _*_
-
-from . import keys
-
-def gen_url() -> str:
-    url: str = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&appid={key}"
-    formatted_url: str = url.format(key=keys.API_KEY, lat=keys.LAT, lon=keys.LON)
-    return formatted_url
