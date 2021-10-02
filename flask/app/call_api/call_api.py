1d18f13259193719a6a323c3f88e13683541f04d
mitty1293 <k.fmichi@gmai.com>
2021-10-03 03:17:16 +0900

    api呼び出し部の実装

--- flask/app/call_api/call_api.py
+++ flask/app/call_api/call_api.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # _*_ coding: utf-8 _*_
 
-import requests, json
+import requests
 from requests.models import Response
 
 def call_api(url: str) -> str:
     response: Response = requests.get(url)
     json_data: dict = response.json()
-    json_text: str = json.dumps(json_data, indent=4)
-    return json_text
+    return json_data
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,6 +1,15 @@
 from app import app
 from flask import render_template, Response
+from call_api import call_api, gen_url
+import json
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/test')
+def test():
+    url = gen_url()
+    data = call_api(url)
+    json_text = json.dumps(data, indent=4)
+    print(json_text)
