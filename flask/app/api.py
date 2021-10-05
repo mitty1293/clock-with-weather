fa138fb2a322f047407fb8630cf0fae2a27ee00c
mitty1293 <k.fmichi@gmai.com>
2021-10-06 00:20:04 +0900

    日本語対応

--- flask/app/api.py
+++ flask/app/api.py
@@ -6,7 +6,7 @@ from requests.models import Response
 from . import keys
 
 def gen_url() -> str:
-    url: str = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&appid={key}"
+    url: str = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&lang=ja&appid={key}"
     formatted_url: str = url.format(key=keys.API_KEY, lat=keys.LAT, lon=keys.LON)
     return formatted_url
 
