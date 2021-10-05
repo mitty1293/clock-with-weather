2d414b9b62ca0dad279f0d42b71de1b6413402c3
mitty1293 <k.fmichi@gmai.com>
2021-10-06 03:33:41 +0900

    fix

--- flask/app/extract_data.py
+++ flask/app/extract_data.py
@@ -2,8 +2,12 @@ import datetime
 
 def of_hourly(**weather_data: dict) :
     for i in range(12):
-        item = weather_data['hourly'][i]
-        dt = datetime.datetime.fromtimestamp(item['dt'])
+        after_i_hour = weather_data['hourly'][i]
+        dt = datetime.datetime.fromtimestamp(after_i_hour['dt'])
+        temp = after_i_hour['temp']
+        icon = after_i_hour['weather'][0]['icon']
+        description = after_i_hour['weather'][0]['description']
+        
 
 def of_daily(**weather_data: dict) :
     pass
