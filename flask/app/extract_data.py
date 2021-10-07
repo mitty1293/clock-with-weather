842792df72620d930e15146ff2ba5c2dc130a5ce
mitty1293 <k.fmichi@gmai.com>
2021-10-07 16:04:59 +0900

    時間ごと天気の表示実装

--- flask/app/extract_data.py
+++ flask/app/extract_data.py
@@ -1,13 +1,25 @@
-import datetime
+from datetime import datetime, timezone, timedelta
+from decimal import Decimal, ROUND_HALF_UP
+
+def convert_unixtime_into_jst(unixtime) -> datetime:
+    return datetime.fromtimestamp(unixtime, timezone(timedelta(hours=9)))
+
+def get_icon_url(icon_id: str) -> str:
+    url: str = "http://openweathermap.org/img/wn/{icon_id}@2x.png"
+    icon_url: str = url.format(icon_id=icon_id)
+    return icon_url
+
+def round_to_int(f: float) -> Decimal:
+    return Decimal(str(f)).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
 
 def of_hourly(**weather_data: dict) -> list:
     hourly_data: list = []
     for i in range(12):
         hourly_property: dict = weather_data['hourly'][i]
         data: dict = {
-            'dt': datetime.datetime.fromtimestamp(hourly_property['dt']),
-            'temp': hourly_property['temp'],
-            'icon': hourly_property['weather'][0]['icon'],
+            'dt': convert_unixtime_into_jst(hourly_property['dt']).strftime('%H:%M'),
+            'temp': round_to_int(hourly_property['temp']),
+            'icon': get_icon_url(hourly_property['weather'][0]['icon']),
             'description': hourly_property['weather'][0]['description'],
             'rain': hourly_property.get('rain', {'1h': 0}).get('1h')
         }
@@ -18,9 +30,9 @@ def of_daily(**weather_data: dict) -> list:
     daily_data: list = []
     for daily_property in weather_data['daily']:
         data: dict = {
-            'dt': datetime.datetime.fromtimestamp(daily_property['dt']),
-            'temp_max': daily_property['temp']['max'],
-            'temp_min': daily_property['temp']['min'],
+            'dt': convert_unixtime_into_jst(daily_property['dt']).strftime('%m/%d %a'),
+            'temp_max': round_to_int(daily_property['temp']['max']),
+            'temp_min': round_to_int(daily_property['temp']['min']),
             'icon': daily_property['weather'][0]['icon'],
             'description': daily_property['weather'][0]['description'],
             'rain': daily_property.get('rain', 0)
